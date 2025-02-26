845891f23807ad39152eb76c33124856257ad91d
samuelkroot <samuel.k.root@gmail.com>
2025-02-26 15:11:38 -0500

    Update post component

--- src/components/Post.jsx
+++ src/components/Post.jsx
@@ -1,7 +1,20 @@
-const Post = () => {
+import styles from '../styles/post.module.css';
+import PropTypes from "prop-types";
+
+const Post = ({votes, poster, title, description, category}) => {
     return (
-        <></>
+        <div>
+            <div></div>
+        </div>
     );
+}
+
+Post.propTypes = {
+    votes: PropTypes.number,
+    poster: PropTypes.string,
+    title: PropTypes.string,
+    description: PropTypes.string,
+    category: PropTypes.string
 }
 
 export default Post;
